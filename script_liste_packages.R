d1faa77feda9545063ea3b4b542ef26b6e84a1bd
Gregoire Le Campion <gregoire.lecampion@hotmail.fr>
2026-01-07 17:27:09 +0100

    ajout liste packages

---
+++ script_liste_packages.R
@@ -0,0 +1,55 @@
+# Liste des packages à installer
+
+
+# Package pour la visualisation et manipulation de données
+install.packages("here")
+install.packages("DT")
+install.packages("dplyr")
+
+# Package pour la représentation statistique
+install.packages("car")
+install.packages("correlation")
+install.packages("corrplot")
+install.packages("ggplot2")
+install.packages("gtsummary")
+install.packages("GGally")
+install.packages("plotly")
+install.packages("ggstatsplot")
+
+# Package pour la manipulation et représentation de la donnée spatiale (cartographie")
+install.packages("sf")
+install.packages("mapsf")
+install.packages("rgeoda") # permet en plus de calculer les indices d'autocorrélation spatiale
+install.packages("RColorBrewer")
+
+# Package pour le calcul du voisinage et réalisation de la GWR
+install.packages("spdep")
+install.packages("GWmodel")
+install.packages("mgwrsar")
+
+# Chargement, visualisation et manipulation de la données
+library(here)
+library(DT)
+library(dplyr)
+
+# Analyse et représentation statistique
+library(car)
+library(correlation)
+library(corrplot)
+library(ggplot2)
+library(gtsummary)
+library(GGally)
+library(plotly)
+library(ggstatsplot)
+
+# Manipulation et représentation de la donnée spatiale (cartographie)
+library(sf)
+library(mapsf)
+library(rgeoda) # permet en plus de calculer les indices d'autocorrélation spatiale
+library(RColorBrewer)
+
+# Calcul du voisinage et réalisation de la GWR
+library(spdep)
+library(GWmodel)
+library(mgwrsar)
+
